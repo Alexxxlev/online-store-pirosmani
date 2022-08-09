df77c2f262d57f8a9e51fe01593b35cdcd46fb92
Alexander <102146514+Alexxxlev@users.noreply.github.com>
2022-08-09 23:58:08 +0300

    добавление модальных окон

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,32 @@
-jQuery(".line-block-buttons .tab-btn-1").on("click",(function(){jQuery(".line-block-buttons button").removeClass("active"),jQuery(this).addClass("active"),jQuery(".line-tab-1").addClass("active"),jQuery(".line-tab-2").removeClass("active")})),jQuery(".line-block-buttons .tab-btn-2").on("click",(function(){jQuery(".line-block-buttons button").removeClass("active"),jQuery(this).addClass("active"),jQuery(".line-tab-2").addClass("active"),jQuery(".line-tab-1").removeClass("active")}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/***/ (function() {
+
+eval("/* Табы для profile.html\r\n========================================================================== */ \r\njQuery('.line-block-buttons .tab-btn-1').on('click', function() {\r\n    jQuery('.line-block-buttons button').removeClass('active');\r\n    jQuery(this).addClass('active');\r\n    jQuery('.line-tab-1').addClass('active');\r\n    jQuery('.line-tab-2').removeClass('active');\r\n});\r\n\r\njQuery('.line-block-buttons .tab-btn-2').on('click', function() {\r\n    jQuery('.line-block-buttons button').removeClass('active');\r\n    jQuery(this).addClass('active');\r\n    jQuery('.line-tab-2').addClass('active');\r\n    jQuery('.line-tab-1').removeClass('active');\r\n});\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/app.js"]();
+/******/ 	
+/******/ })()
+;
